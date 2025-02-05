e17b9ba798dbfef1e345bacfda0ee86e42db94a7
yazidoxx <yazid.zalai.yazid@gmail.com>
2025-02-05 13:59:00 +0100

    Move import statements to the top of the script

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,12 +1,17 @@
+import json
+import csv
+import datetime as dt
+import matplotlib.pyplot as plt
+
 # https://data.nasa.gov/resource/eva.json (with modifications)
-data_f = open('./eva_data.json', 'r')
-data_t = open('./eva_data.csv','w')
-g_file = 'myplot.png'
+data_f = open('./eva-data.json', 'r')
+data_t = open('./eva-data.csv','w')
+g_file = './cumulative_eva_graph.png' 
+
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
 data=[]
-import json
 
 for i in range(374):
     line=data_f.readline()
@@ -14,11 +19,8 @@ for i in range(374):
     data.append(json.loads(line[1:-1]))
 #data.pop(0)
 ## Comment out this bit if you don't want the spreadsheet
-import csv
 
 w=csv.writer(data_t)
-
-import datetime as dt
 
 time = []
 date =[]
@@ -50,8 +52,6 @@ for i in time:
     t.append(t[-1]+i)
 
 date,time = zip(*sorted(zip(date, time)))
-
-import matplotlib.pyplot as plt
 
 plt.plot(date,t[1:], 'ko-')
 plt.xlabel('Year')
